56e34a5b0224a73083f7e85676c5abb591e4d0dd
codersign <regiebaquero@gmail.com>
2022-08-23 10:01:27 +0800

    Add project files.

---
+++ AutomapperWebAPI/Profile/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using AutomapperWebAPI.Entities;
+using AutomapperWebAPI.DTO;
+//using AutomapperWebAPI.HelperFunctions;
+namespace AutomapperWebAPI.MappingProfile
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserReadDto>()
+               .ForMember(
+                   dest => dest.FullName,
+                   opt => opt.MapFrom(nms => $"{nms.FirstName} {nms.LastName}"));
+               
+
+            CreateMap<UserCreateDto, User>();
+        }
+    }
+}
